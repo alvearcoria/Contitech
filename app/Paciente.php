0babf3b7dbdf2f297cbd51f6be18e6ed369ac27f
Manuel Esquivel <manuel_1155@hotmail.com>
2018-07-12 14:16:20 -0500

    CRUD Paciente

---
+++ app/Paciente.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Kyslik\ColumnSortable\Sortable;
+
+class Paciente extends Model
+{
+	use Sortable;
+    protected $table = 'pacientes';
+    protected $primaryKey = 'id_paciente'; 
+
+    public $sortable = ['id_paciente', 'nombre_pac'];
+
+    public function scopeName($query, $name){
+		if(trim($name!="")){
+			$query->where("nombre_pac","like","%".$name."%");
+		}
+	}
+}
---
+++ resources/views/pacientes/create.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.sidebar')
+
+
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2>Registrar nuevo paciente</h2>
+            </div>
+                <a class="btn btn-danger" href="{{ route('pacientes.index') }}"><span class="fas fa-arrow-alt-circle-left"></span> Regresar</a>
+        </div>
+    </div>
+
+    @if (count($errors) < 0)
+        <div class="alert alert-danger">
+            <strong>Whoops!</strong> There were some problems with your input.<br><br>
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+
+    {!! Form::open(array('route' => 'pacientes.store','method'=>'POST')) !!}
+         @include('pacientes.form')
+    {!! Form::close() !!}
+
+
+@endsection
